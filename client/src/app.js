cd96a4fd45982c0cec6943ac6774d5a467135a5d
goobering <goobering@gmail.com>
2017-08-11 11:47:10 +0100

    Added JS code for creating a map on page load

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,15 @@ var app = function(){
     // var exampleElement = document.querySelector('#some-element');
     // Set the onchange method of the element to onExampleElementChanged
     // exampleElement.onchange = onExampleElementChanged;
+
+    createMap();
+}
+
+var createMap = function(){
+    var mapDiv = document.querySelector('#map');
+    var center = {lat: 54.606520, lng: -2.547321};
+    var zoom = 18;
+    var mainMap = new MapWrapper(mapDiv, center, zoom);
 }
 
 // var onExampleElementChanged = function(){
---
+++ client/src/mapWrapper.js
@@ -0,0 +1,48 @@
+var MapWrapper = function(container, center, zoomLevel){
+    this.googleMap = new google.maps.Map(container, {
+        center: center,
+        zoom: zoomLevel
+    });
+    this.markers = [];
+};
+
+MapWrapper.prototype.addMarker = function(coords, map){
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: this.googleMap
+    });
+
+    marker.addListener('click', this.handleMarkerClick);
+    this.markers.push(marker);
+}
+
+MapWrapper.prototype.addClickEvent = function(){
+    google.maps.event.addListener(this.googleMap, 'click', function(event){
+        console.log(event.latLng.lng());
+        console.log(event.latLng.lat());
+
+        var coords = {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng()
+        };
+
+        // this.addMarker(coords, this.googleMap);
+    }.bind(this));
+};
+
+MapWrapper.prototype.bounceMarkers = function(){
+    this.markers.forEach(function(marker){
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    });
+};
+
+
+// CORS request code based on: https://www.html5rocks.com/en/tutorials/cors/ 
+// Lightbulb/facepalm moment provided by: https://stackoverflow.com/a/23952300
+MapWrapper.prototype.handleMarkerClick = function(){
+    
+};
+
+MapWrapper.prototype.setCenter = function(coords){
+    this.googleMap.setCenter(coords);
+};
