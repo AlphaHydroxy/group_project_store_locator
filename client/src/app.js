6b61d86323252caf2c14ea459ce61d4b0b49b736
David Clarkson <clarkee013@gmail.com>
2017-08-14 14:08:52 +0100

    mostly completed on the list of detailsf or each pub, slight formatting of the list required and check on the details appearing outside of the results box

--- client/src/app.js
+++ client/src/app.js
@@ -57,23 +57,103 @@ function getPlaceChangedHandler(autoCompleteBox, mapWrapper){
         mapWrapper.setZoom(12);
         mapWrapper.clearMarkers();
         mapWrapper.addMarker(place.geometry.location);
-debugger;
         fetchVenues(place.geometry.location, mapWrapper);
     }
 }
 
 function fetchVenues(position, mapWrapper){
-    debugger;
     var venues = new Venues();
     venues.nearby(position, function(result){        
         var jsonString = this.responseText;
         var results = JSON.parse(jsonString);
 
+        var container = document.querySelector('#pub-list-flex-container');
+        container.innerHTML = "";
         results.near.forEach(function(venue){
             mapWrapper.addMarker({lat: venue.coords.lat, lng: venue.coords.long});
+            createVenueResultList(venue);
         })
     });
 }
+
+function createVenueResultList(venue){
+    var pubListContainer = document.querySelector('#pub-list-flex-container');
+    
+    var h1 = document.createElement("h1");
+    h1.innerText = venue.name;
+
+    var ulPubDetails = document.createElement("ul");
+
+    var li1Add1 = document.createElement("li");
+    li1Add1.innerHTML = venue.addressLine1;
+    var li2Add2 = document.createElement("li");
+    li2Add2.innerHTML = venue.addressLine2;
+    var li3Town = document.createElement("li");
+    li3Town.innerHTML = venue.town;
+    var li4Region = document.createElement("li");
+    li4Region.innerHTML = venue.region;
+    var li5Postcode = document.createElement("li");
+    li5Postcode.innerHTML = venue.postCode;
+    var li6Phone = document.createElement("li");
+    li6Phone.innerHTML = venue.phone;
+    var li7Email = document.createElement("li");
+    li7Email.innerHTML = venue.email;
+    var li8Facilities = document.createElement("li");
+    li8Facilities.innerHTML = venue.facilites;
+
+    var openingTimesButtonContainer = document.createElement("div")
+    openingTimesButtonContainer.id = "btn-container";
+
+    var actualButton = document.createElement("button");
+    actualButton.onclick = "toggleOpeningHours('opening-hours-panel')";
+    actualButton.innerHTML = "More times...<span id='arrow'>&#9662</span>"
+
+    var openingTimesContainer = document.createElement("div");
+    openingTimesContainer.id = "opening-hours-panel";
+
+    var ulOpeningTimes = document.createElement("ul");
+    ulOpeningTimes.id = "slide-panel";
+
+    var openingMonday = document.createElement("li");
+    openingMonday.innerHTML = venue.openingTimes.monday[0] + " : " + venue.openingTimes.monday[1];
+    var openingTuesday = document.createElement("li");
+    openingTuesday.innerHTML = venue.openingTimes.tuesday[0] + " : " + venue.openingTimes.tuesday[1];
+    var openingWednesday = document.createElement("li");
+    openingWednesday.innerHTML = venue.openingTimes.wednesday[0] + " : " + venue.openingTimes.wednesday[1];
+    var openingThursday = document.createElement("li");
+    openingThursday.innerHTML = venue.openingTimes.thursday[0] + " : " + venue.openingTimes.thursday[1];
+    var openingFriday = document.createElement("li");
+    openingFriday.innerHTML = venue.openingTimes.friday[0] + " : " + venue.openingTimes.friday[1];
+    var openingSaturday = document.createElement("li");
+    openingSaturday.innerHTML = venue.openingTimes.saturday[0] + " : " + venue.openingTimes.saturday[1];
+    var openingSunday = document.createElement("li");
+    openingSunday.innerHTML = venue.openingTimes.sunday[0] + " : " + venue.openingTimes.sunday[1];
+
+    pubListContainer.appendChild(h1);
+    pubListContainer.appendChild(ulPubDetails);
+    ulPubDetails.appendChild(li1Add1)
+    ulPubDetails.appendChild(li2Add2);
+    ulPubDetails.appendChild(li3Town);
+    ulPubDetails.appendChild(li4Region);
+    ulPubDetails.appendChild(li5Postcode);
+    ulPubDetails.appendChild(li6Phone);
+    ulPubDetails.appendChild(li7Email);
+    ulPubDetails.appendChild(li8Facilities);
+
+    ulPubDetails.appendChild(openingTimesButtonContainer);
+    openingTimesButtonContainer.appendChild(actualButton);
+   
+    ulPubDetails.appendChild(openingTimesContainer);
+    ulOpeningTimes.appendChild(openingMonday);
+    ulOpeningTimes.appendChild(openingTuesday);
+    ulOpeningTimes.appendChild(openingWednesday);
+    ulOpeningTimes.appendChild(openingThursday);
+    ulOpeningTimes.appendChild(openingFriday);
+    ulOpeningTimes.appendChild(openingSaturday);
+    ulOpeningTimes.appendChild(openingSunday);
+    openingTimesContainer.appendChild(ulOpeningTimes);
+}
+
 
 
 
